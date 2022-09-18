3caf10950d4df7d839628b38212be442004def29
Henry <henrykc24@gmail.com>
2022-09-18 12:39:04 +0100

    Add list of currencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  return <div>Currency Converter</div>;
+  return <div>Currency Converter<img src="https://flagcdn.com/48x36/us.png"></img></div>;
 };
 
 export default App;
