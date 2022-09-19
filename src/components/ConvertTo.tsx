999a6a4845e7d54eb33f9a31e4bcb3c043743829
Henry <henrykc24@gmail.com>
2022-09-20 00:02:07 +0100

    Finish off ConvertTo component logic

--- src/components/ConvertTo.tsx
+++ src/components/ConvertTo.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import currencies from "../utils/currencies.json";
+import getRates from "../utils/getRates";
 import CountryFlag from "./CountryFlag";
 import CurrencyListItem from "./CurrencyListItem";
 
@@ -7,25 +8,43 @@ const ConvertTo = ({
   show2,
   setShow1,
   setShow2,
+  setExchRate2,
+  exchRate1,
+  exchRate2,
+  setValue1,
+  setValue2,
+  value2,
 }: {
+  setExchRate2: React.Dispatch<React.SetStateAction<number>>;
+  exchRate1: number;
+  exchRate2: number;
+  setValue1: React.Dispatch<React.SetStateAction<number>>;
+  setValue2: React.Dispatch<React.SetStateAction<number>>;
+  value2: number;
   show2: boolean;
   setShow1: React.Dispatch<React.SetStateAction<boolean>>;
   setShow2: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  // const [show, setShow] = useState(false);
   const [defaultCurrency, setDefaultCurrency] = useState(() =>
     currencies.find((curr) => curr.currency === "USD")
   );
-  console.log("default", defaultCurrency);
+
+  useEffect(() => {
+    getRates("http://localhost:4000/api/v1/rates/", "USD", setExchRate2);
+  }, [setExchRate2]);
+
   return (
     <div className="parent-from">
       <div>
         Currency
-        <div onClick={(e) => {
-          e.stopPropagation();
-          setShow2((prev) => !prev);
-          setShow1(() => false);
-        }} className="currency-card">
+        <div
+          onClick={(e) => {
+            e.stopPropagation();
+            setShow2((prev) => !prev);
+            setShow1(() => false);
+          }}
+          className="currency-card"
+        >
           <div className="flag-name">
             <div className="flag">
               <CountryFlag countryAlpha2Code={defaultCurrency?.countryCode} />
@@ -45,32 +64,47 @@ const ConvertTo = ({
             ></i>
           </div>
         </div>
-        {(show2 || true) && (
-          <div className={`dropdown ${show2 ? "show" : ""}`}>
-            <div onClick={(e) => e.stopPropagation()}>
-              <i className="fa fa-search"></i>
-              <input type="text" />
-            </div>
-            <div className="list">
-              {currencies.map((item) => (
-                <CurrencyListItem
-                  setDefaultCurrency={setDefaultCurrency}
-                  setShow={setShow2}
-                  alphaCode={item.countryCode}
-                  currencyName={item.currency}
-                  currencyDescription={item.currencyName}
-                  currencySymbol={item.currencySymbol}
-                />
-              ))}
-            </div>
+        <div className={`dropdown ${show2 ? "show" : ""}`}>
+          <div onClick={(e) => e.stopPropagation()}>
+            <i className="fa fa-search"></i>
+            <input type="text" />
           </div>
-        )}
+          <div className="list">
+            {currencies.map((item) => (
+              <CurrencyListItem
+                setValue1={setValue1}
+                setValue2={setValue2}
+                setExchRate={setExchRate2}
+                setDefaultCurrency={setDefaultCurrency}
+                setShow={setShow2}
+                alphaCode={item.countryCode}
+                currencyName={item.currency}
+                currencyDescription={item.currencyName}
+                currencySymbol={item.currencySymbol}
+              />
+            ))}
+          </div>
+        </div>
       </div>
       <div className="input-amount1">
         <span>Amount</span>
         <div className="input">
           <span>{defaultCurrency?.currencySymbol}</span>
-          <input type="number" />
+          <input
+            value={value2.toString()}
+            onChange={(e) => {
+              setValue2(() => Number(e.target.value));
+              const curr = defaultCurrency?.currency;
+              if (curr === "AUD") {
+                const x = Number(e.target.value) * exchRate1;
+                setValue1(() => +x.toFixed(2));
+              } else {
+                const x = (Number(e.target.value) / exchRate2) * exchRate1;
+                setValue1(() => +x.toFixed(2));
+              }
+            }}
+            type="number"
+          />
           <span>{defaultCurrency?.currency}</span>
         </div>
       </div>
