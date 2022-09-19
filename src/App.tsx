8da912bda4d8016ff068c08ea5484948a2f68672
Henry <henrykc24@gmail.com>
2022-09-19 14:45:32 +0100

    Add components to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ConvertTo from "./components/ConvertTo";
+import ConvertFrom from "./components/ConvertFrom";
 
 const App: React.FC = () => {
-  return <div>Currency Converter<img src="https://flagcdn.com/48x36/us.png"></img></div>;
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  return (
+    <div className="main" onClick={() => {
+      setShow1(() => false);
+      setShow2(() => false);
+    }}>
+      <ConvertFrom
+        show1={show1}
+        setShow1={setShow1}
+        setShow2={setShow2}
+      />
+      <ConvertTo
+        show2={show2}
+        setShow1={setShow1}
+        setShow2={setShow2}
+      />
+    </div>
+  );
 };
 
 export default App;
